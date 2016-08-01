a3b118572daa794327450485d57845b3a165ceee
Tom Work <tj2huang@uwaterloo.ca>
2016-08-01 16:17:46 -0400

    june sept comparison

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -7,8 +7,81 @@ import pickle
 import pandas as pd
 import datetime
 
+alc_keywords = '''drink drinker drinks drinking drank wine champgne alcohol alcoholics alcoholism beer beers bottle bottles
+    pint pints cocktail cocktails bar brewery lounge pub liquor booze vodka tequila gin ciroc margarita margaritas shot shots
+    ale whiskey lager tipsy drunk sober wasted pregame pregaming'''
+
+
+def re_filter(text):
+    """
+    Regular expression matching
+    :param text: string
+    :return: bool if any alcohol keywords are in the text
+    """
+    expression = alc_keywords.split().join('|')
+    alcohol_filter = re.compile(expression)
+    return bool(alcohol_filter.match(text))
+
+
+def prefilter(text):
+    """
+    Keyword matching
+    :param text:
+    :return:
+    """
+    keywords = set(alc_keywords.split())
+    return bool(set(text.split()).intersection(keywords))
+
+
+def sample(in_folder, out_folder, block_size=100000, fraction=0.01):
+    """
+    Samples from csvs
+    :param in_folder: directory of csvs
+    :param out_folder:
+    :param block_size: tweets per csv in output
+    :param fraction: sampling fraction
+    """
+    tweets = pd.DataFrame()
+    file_num = 0
+    for file in os.listdir(in_folder):
+        try:
+            _df = pd.read_csv(in_folder + '/'  + file, encoding='utf8', engine='python')
+            _df_sample = _df.sample(int(len(_df)*fraction))
+            _df['id'].apply(str)
+            tweets = pd.concat([tweets, _df_sample])
+
+            if len(tweets) > block_size:
+                tweets[:block_size].to_csv(out_folder + '/random_' + str(file_num) +'.csv', encoding='utf8')
+                tweets = tweets[block_size:]
+                file_num += 1
+        except Exception as e:
+            print (file)
+            print (e)
+    tweets.to_csv(out_folder + '/tweets_' + str(file_num) +'.csv')
+
+
+def block(in_folder, out_folder, block_size=100000):
+    """
+    Combines csvs into equally sized chunks
+    :param in_folder: directory of unevenly sized csvs
+    :param out_folder:
+    :param block_size:
+    """
+    tweets = pd.DataFrame()
+    file_num = 0
+    for file in os.listdir(in_folder):
+        _df = pd.read_csv(in_folder + '/'  + file)[['id', 'created_at', 'utc_offset', 'text']]
+        tweets = pd.concat([tweets, _df])
+        while len(tweets) > block_size:
+            tweets[:block_size].to_csv(out_folder + '/tweets_' + str(file_num) +'.csv', encoding='utf8')
+            tweets = tweets[block_size:]
+            file_num += 1
+    tweets.to_csv(out_folder + '/tweets_' + str(file_num) +'.csv')
+
+
 def make_batch(pickled_df, size, output_dir):
     """
+    Creates csv to be labeled on AMT
     pickled_df: unlabeled data with 'random_number' column
     size: # of items batch to be created
     output_dir: where the outputs are saved
