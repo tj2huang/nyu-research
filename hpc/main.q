9bd4259d2516ea41030ea7d6d17e7d86da4a3919
Tom Work <tj2huang@uwaterloo.ca>
2016-08-09 17:41:44 -0400

    state based timezone, should work better

--- hpc/main.q
+++ hpc/main.q
@@ -1,23 +1,28 @@
 #PBS -l nodes=1:ppn=16
-#PBS -l walltime=8:00:00
+#PBS -l walltime=1:30:00
 #PBS -l mem=12GB
 #PBS -M tom.huang@nyu.edu
 #PBS -j oe
 
+PROCESSES=16
+
 module purge
-module load anaconda/2.3.0
 module load python3/intel/3.5.1
 SRCDIR=$HOME/nyu-twipsy
 
-RUNDIR=$SCRATCH/nyu-twipsy/run-${PBS_JOBID/.*}
+RUNDIR=$SCRATCH/nyu-twipsy/run-june-under-${PBS_JOBID/.*}
 mkdir -p $RUNDIR/
 
-cp -r $SRCDIR $RUNDIR/
-cd $RUNDIR
+#cp -r $SRCDIR $RUNDIR/
+#cd $RUNDIR
 
-PROJECT=$RUNDIR/nyu-twipsy
-INFOLDER=$SCRATCH/nyu-twipsy/data/june/
+mkdir $RUNDIR/out
+mkdir $RUNDIR/summary
 
-PYTHONPATH=$PROJECT python3 $PROJECT/hpc/main.py $PROJECT/hpc/classifiers/clf_alc_UPDATED.p $PROJECT/hpc/classifiers/clf_fpa_UPDATED.p $PROJECT/hpc/classifiers/clf_fpl_double_labeled $INFOLDER $PROJECT/hpc/out 16
-PYTHONPATH=$PROJECT python3 $PROJECT/hpc/postprocessing.py $PROJECT/hpc/out $PROJECT/hpc/summary 16
+#PROJECT=$RUNDIR/nyu-twipsy"
+PROJECT=$SRCDIR
+INFOLDER=$SCRATCH/nyu-twipsy/data/june-age/under
+
+PYTHONPATH=$PROJECT python3 $PROJECT/hpc/main.py $INFOLDER $RUNDIR/out $PROCESSES
+PYTHONPATH=$PROJECT python3 $PROJECT/hpc/postprocessing.py $RUNDIR/out $RUNDIR/summary $PROCESSES
 
---
+++ hpc/preprocessing/timezone.py
@@ -0,0 +1,156 @@
+import datetime
+
+state2tz= {
+ 'AK': -9,
+ 'AL': -6,
+ 'AR': -6,
+ 'AZ': -7,
+ 'CA': -8,
+ 'CO': -7,
+ 'CT': -5,
+ 'DC': -5,
+ 'DE': -5,
+ 'FL': -5,
+ 'GA': -5,
+ 'HI': -10,
+ 'IA': -6,
+ 'ID': -7,
+ 'IL': -6,
+ 'IN': -5,
+ 'KS': -6,
+ 'KY': -5,
+ 'LA': -6,
+ 'MA': -5,
+ 'MD': -5,
+ 'ME': -5,
+ 'MI': -5,
+ 'MN': -6,
+ 'MO': -6,
+ 'MS': -6,
+ 'MT': -7,
+ 'NC': -5,
+ 'ND': -6,
+ 'NE': -6,
+ 'NH': -5,
+ 'NJ': -5,
+ 'NM': -7,
+ 'NV': -8,
+ 'NY': -5,
+ 'OH': -5,
+ 'OK': -6,
+ 'OR': -8,
+ 'PA': -5,
+ 'RI': -5,
+ 'SC': -5,
+ 'SD': -6,
+ 'TN': -6,
+ 'TX': -6,
+ 'UT': -7,
+ 'VA': -5,
+ 'VT': -5,
+ 'WA': -8,
+ 'WI': -6,
+ 'WV': -5,
+ 'WY': -7}
+
+states = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+}
+
+states_inverted = {v:k for (k,v) in states.items()}
+
+
+def map2name(s):
+    if "USA" in s:
+        state = s[:-5]
+        if state in states_inverted:
+            return states_inverted[state]
+        else:
+            return "<Other>"
+    try:
+        state_code = s[-2:]
+        if state_code in states:
+            return state_code
+    except:
+        return "<Other>"
+    return "<Other>"
+
+
+def map2tz(s):
+    try:
+        return state2tz[s]
+    except:
+        return 0
+
+
+def convert2local(df):
+    # df: index is datetime, has "place" column
+    # new years dataset
+
+    # convert the place name into a state or <other>
+    df["location"] = df.place.astype(str).apply(map2name)
+
+    # convert the state to a timedelta object
+    df["tz"] = df.location.apply(map2tz).apply(lambda _: datetime.timedelta(hours=_))
+
+    # modify the index to account for the timedelta
+    df['local_time'] = df.index + df["tz"]
+
+    # filter data only that have state data
+    return df[df["location"] != "<Other>"]
